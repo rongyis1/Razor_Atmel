dc9b9219ee690a2439bd5e8108fa26ed5c28ce7c
RongYis <1328852641@qq.com>
2018-04-11 15:56:21 +0800

    FW2_Asssignment2_Ant_master
    figure out how to send acknowledge data instead of Broadcast messages
    and change au8TestMessage bytes' meaning, then display total messages
    sent and missed messages on the LCD.

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -52,14 +52,18 @@ extern volatile u32 G_u32ApplicationFlags;             /* From main.c */
 extern volatile u32 G_u32SystemTime1ms;                /* From board-specific source file */
 extern volatile u32 G_u32SystemTime1s;                 /* From board-specific source file */
 
+extern u32 G_u32AntApiCurrentMessageTimeStamp;                    /* From ant_api.c */
+extern AntApplicationMessageType G_eAntApiCurrentMessageClass;    /* From ant_api.c */
+extern u8 G_au8AntApiCurrentMessageBytes[ANT_APPLICATION_MESSAGE_BYTES];  /* From ant_api.c */
+extern AntExtendedDataType G_sAntApiCurrentMessageExtData;                /* From ant_api.c */
 
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
 Variable names shall start with "UserApp1_" and be declared as static.
 ***********************************************************************************************************************/
 static fnCode_type UserApp1_StateMachine;            /* The state machine function pointer */
-//static u32 UserApp1_u32Timeout;                      /* Timeout counter used across states */
-
+static u32 UserApp1_u32Timeout;                      /* Timeout counter used across states */
+static AntAssignChannelInfoType UserApp1_sChannelInfo; 
 
 /**********************************************************************************************************************
 Function Definitions
@@ -87,17 +91,44 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  u8 au8WelcomeMessage[] = "ANT Master";
+
+  /* Set a message up on the LCD. Delay is required to let the clear command send. */
+  LCDCommand(LCD_CLEAR_CMD);
+  for(u32 i = 0; i < 10000; i++);
+  LCDMessage(LINE1_START_ADDR, au8WelcomeMessage);
+
+  /* Configure ANT for this application */
+  UserApp1_sChannelInfo.AntChannel          = ANT_CHANNEL_USERAPP;
+  UserApp1_sChannelInfo.AntChannelType      = ANT_CHANNEL_TYPE_USERAPP;
+  UserApp1_sChannelInfo.AntChannelPeriodLo  = ANT_CHANNEL_PERIOD_LO_USERAPP;
+  UserApp1_sChannelInfo.AntChannelPeriodHi  = ANT_CHANNEL_PERIOD_HI_USERAPP;
  
-  /* If good initialization, set state to Idle */
-  if( 1 )
+  UserApp1_sChannelInfo.AntDeviceIdLo       = ANT_DEVICEID_LO_USERAPP;
+  UserApp1_sChannelInfo.AntDeviceIdHi       = ANT_DEVICEID_HI_USERAPP;
+  UserApp1_sChannelInfo.AntDeviceType       = ANT_DEVICE_TYPE_USERAPP;
+  UserApp1_sChannelInfo.AntTransmissionType = ANT_TRANSMISSION_TYPE_USERAPP;
+  UserApp1_sChannelInfo.AntFrequency        = ANT_FREQUENCY_USERAPP;
+  UserApp1_sChannelInfo.AntTxPower          = ANT_TX_POWER_USERAPP;
+
+  UserApp1_sChannelInfo.AntNetwork = ANT_NETWORK_DEFAULT;
+  for(u8 i = 0; i < ANT_NETWORK_NUMBER_BYTES; i++)
   {
-    UserApp1_StateMachine = UserApp1SM_Idle;
+    UserApp1_sChannelInfo.AntNetworkKey[i] = ANT_DEFAULT_NETWORK_KEY;
+  }
+  
+  /* Attempt to queue the ANT channel setup */
+  if( AntAssignChannel(&UserApp1_sChannelInfo) )
+  {
+    UserApp1_u32Timeout = G_u32SystemTime1ms;
+    UserApp1_StateMachine = UserApp1SM_AntChannelAssign;
   }
   else
   {
     /* The task isn't properly initialized, so shut it down and don't run */
     UserApp1_StateMachine = UserApp1SM_Error;
   }
+
 
 } /* end UserApp1Initialize() */
 
@@ -131,11 +162,87 @@ void UserApp1RunActiveState(void)
 /**********************************************************************************************************************
 State Machine Function Definitions
 **********************************************************************************************************************/
+/* Wait for ANT channel assignment */
+static void UserApp1SM_AntChannelAssign(void)
+{
+  if( AntRadioStatusChannel(ANT_CHANNEL_USERAPP) == ANT_CONFIGURED)
+  {
+    /* Channel assignment is successful, so open channel and
+    proceed to Idle state */
+    AntOpenChannelNumber(ANT_CHANNEL_USERAPP);
+    UserApp1_StateMachine = UserApp1SM_Idle;
+    /*LCD message display*/
+    LCDCommand(LCD_CLEAR_CMD);
+    LCDMessage(LINE1_START_ADDR, "Tot:");
+    LCDMessage(LINE2_START_ADDR, "Mis:no failed");
+  }
+  
+  /* Watch for time out */
+  if(IsTimeUp(&UserApp1_u32Timeout, 3000))
+  {
+    UserApp1_StateMachine = UserApp1SM_Error;    
+  }
+       
+} /* end UserApp1SM_AntChannelAssign */
 
 /*-------------------------------------------------------------------------------------------------------------------*/
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
+  static u8 u8State;
+  static u8 au8TestMessage[] = {0x5B, 0, 0, 0, 0xFF, 0, 0, 0};
+  static u8 au8DataContent[] = "xxxxxxxxxxxxxxxx";
+  
+  if( AntReadAppMessageBuffer() )
+  {
+    /*read ANT_TICK to determine if the slave received ACK message*/
+    if(G_eAntApiCurrentMessageClass == ANT_TICK)
+    {
+
+     /* Update and queue the new message data */
+      au8TestMessage[7]++;
+      if(au8TestMessage[7] == 0)
+      {
+        au8TestMessage[6]++;
+        if(au8TestMessage[6] == 0)
+        {
+          au8TestMessage[5]++;
+        }
+      }
+      
+      /*turn hex to ascii char which can display on LCD*/
+      for(u8 i = 0; i < ANT_DATA_BYTES; i++)
+      {    
+        au8DataContent[2 * i]     = HexToASCIICharUpper(au8TestMessage[i] / 16);
+        au8DataContent[2 * i + 1] = HexToASCIICharUpper(au8TestMessage[i] % 16);
+      }
+      LCDMessage(LINE1_START_ADDR+4, au8DataContent);  //display total messages
+      
+      u8State = G_au8AntApiCurrentMessageBytes[ANT_TICK_MSG_EVENT_CODE_INDEX];
+      /*the slave does not receive the message,
+      increment the missed messages counter*/
+      if(u8State == EVENT_TRANSFER_TX_FAILED)
+      {   
+        LCDMessage(LINE2_START_ADDR+4, au8DataContent);//display missed messages
+        
+        au8TestMessage[3]++;
+        if(au8TestMessage[3] == 0)
+        {
+          au8TestMessage[2]++;
+          if(au8TestMessage[2] == 0)
+          {
+            au8TestMessage[1]++;
+          }
+        }
+      }
+      
+      
+
+      
+      AntQueueAcknowledgedMessage(ANT_CHANNEL_USERAPP, au8TestMessage);
+    }
+    
+  } /* end AntReadData() */
 
 } /* end UserApp1SM_Idle() */
     
--- firmware_common/application/user_app1.h
+++ firmware_common/application/user_app1.h
@@ -28,7 +28,17 @@ Type Definitions
 /**********************************************************************************************************************
 Constants / Definitions
 **********************************************************************************************************************/
-
+/* Required constants for ANT channel configuration */
+#define ANT_CHANNEL_TYPE_USERAPP        CHANNEL_TYPE_MASTER   // Device is a MASTER
+#define ANT_CHANNEL_USERAPP             ANT_CHANNEL_0         // Channel 0 - 7
+#define ANT_DEVICEID_LO_USERAPP         (u8)0x34              // Low byte of two-byte Device #
+#define ANT_DEVICEID_HI_USERAPP         (u8)0x12              // High byte of two-byte Device #
+#define ANT_DEVICE_TYPE_USERAPP         (u8)1                 // 1 - 255
+#define ANT_TRANSMISSION_TYPE_USERAPP   (u8)1                 // 1-127 (MSB is pairing bit)
+#define ANT_CHANNEL_PERIOD_LO_USERAPP   (u8)0x00              // Low byte of two-byte channel period 0x0001 - 0x7fff
+#define ANT_CHANNEL_PERIOD_HI_USERAPP   (u8)0x20              // High byte of two-byte channel period 
+#define ANT_FREQUENCY_USERAPP           (u8)50                // 2400MHz + this number 0 - 99
+#define ANT_TX_POWER_USERAPP            RADIO_TX_POWER_4DBM   // RADIO_TX_POWER_0DBM, RADIO_TX_POWER_MINUS5DBM, RADIO_TX_POWER_MINUS10DBM, RADIO_TX_POWER_MINUS20DBM
 
 /**********************************************************************************************************************
 Function Declarations
@@ -54,6 +64,8 @@ void UserApp1RunActiveState(void);
 /***********************************************************************************************************************
 State Machine Declarations
 ***********************************************************************************************************************/
+static void UserApp1SM_AntChannelAssign(void);
+
 static void UserApp1SM_Idle(void);    
 
 static void UserApp1SM_Error(void);         
